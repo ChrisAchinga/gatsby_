54a5ed087982d811090db6e50560ad56c3688496
ChrisAchinga <achinga.chris@gmail.com>
2020-07-20 18:06:52 +0300

    random imaage from unsplash:

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ export default function Home() {
     <div style={{ color: `purple` }}>
       <h1>Hey Jamila</h1>
       <p>I love you</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
   );
 }
