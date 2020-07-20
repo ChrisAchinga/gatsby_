8557c56d9f8f4e92070bf2e75673768915b6eec6
ChrisAchinga <achinga.chris@gmail.com>
2020-07-20 18:05:36 +0300

    hello Jamila

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
 
 export default function Home() {
-  return <div style={{ color: `purple`, fontSize: `72px` }}>Whatsup Jamila</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>Hey Jamila</h1>
+      <p>I love you</p>
+    </div>
+  );
 }
