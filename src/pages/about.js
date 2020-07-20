ff8d43922ee2d46a32e7dec0df39ad8f4deec663
ChrisAchinga <achinga.chris@gmail.com>
2020-07-20 18:48:48 +0300

    pages routing and props

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,7 @@ export default function About() {
   return (
     <div style={{ color: `teal` }}>
       <Header headerText="About Jamila" />
+      <Header headerText="She is cool and pretty" />
       <p>Such wow. Very React.</p>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
   return (
     <div style={{ color: `purple` }}>
+      <Link to="/contact/">Contact</Link>
       <h1>Hey Jamila</h1>
       <p>I love you</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
