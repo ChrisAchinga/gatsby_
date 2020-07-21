2f8971be17b513ebeb5dd85660e565e8fd99fcb4
ChrisAchinga <achinga.chris@gmail.com>
2020-07-21 16:42:10 +0300

    use nested layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 
-export default function About() {
+export default function About({ data }) {
   return (
     <Layout>
+       <h1>About {data.site.siteMetadata.title}</h1>
     <div>
       <h1>About me</h1>
       <p>
