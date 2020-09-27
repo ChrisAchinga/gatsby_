23117f68e3193cffba4cdc0274144062819a36cd
ChrisAchinga <achinga.chris@gmail.com>
2020-09-27 20:13:43 +0300

    server

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ const path = require('path')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
+    if (req.url == '/') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), 
+        (err, content) => {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content);
+        })
+    }
+    if (req.url == '/about') {
+        fs.readFile(path.join(__dirname, 'public', 'about.html'), 
+        (err, content) => {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content);
+        })
+    }
 });
 
 const PORT = process.env.PORT || 3000;
