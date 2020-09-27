5d8c5b5b555cee2280cb2c80e99a73fbfc8a4284
ChrisAchinga <achinga.chris@gmail.com>
2020-09-27 20:17:29 +0300

    rest api mock

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
-const http = require('http')
-const path = require('path')
-const fs = require('fs')
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    if (req.url == '/') {
-        fs.readFile(path.join(__dirname, 'public', 'index.html'), 
-        (err, content) => {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(content);
-        })
-    }
-    if (req.url == '/about') {
-        fs.readFile(path.join(__dirname, 'public', 'about.html'), 
-        (err, content) => {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(content);
-        })
-    }
+  if (req.url == "/") {
+    fs.readFile(
+      path.join(__dirname, "public", "index.html"),
+      (err, content) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
+  if (req.url == "/about") {
+    fs.readFile(
+      path.join(__dirname, "public", "about.html"),
+      (err, content) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
+  if (req.url == "/api/users") {
+    const users = [
+        {name: 'John Doe', age: 22},
+        {name: 'Janet Doe', age: 23}
+    ];
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(users));
+  }
 });
 
 const PORT = process.env.PORT || 3000;
